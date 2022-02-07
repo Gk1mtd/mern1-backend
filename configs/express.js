b0e657e49db155e53738bb6b3919b86e886ecd19
Michael Seitz <michael.s.seitz@gmail.com>
2022-02-07 18:48:39 +0100

    with sessions
    Co-authored-by: Philipp Kastl <philippkastl@gmail.com>

--- configs/express.js
+++ configs/express.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-const { PORT, ORIGIN } = process.env;
+const sessions = require("express-session");
+const MongoStore = require("connect-mongo");
+const { PORT, ORIGIN, SECRET, MONGO_URL, NODE_ENV } = process.env;
 
 async function startExpress(app) {
   try {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cors({ credentials: true, origin: ORIGIN }));
+    // session settings
+    app.set('trust proxy', 1);
+    app.use(
+      sessions({
+        secret: SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+          mongoUrl: MONGO_URL,
+        }),
+        cookie: {
+          maxAge: 1000 * 60,
+          sameSite: false,
+          secure: NODE_ENV === "production",
+        },
+      })
+    );
 
     app.listen(PORT, () => console.log(`Hey, Listen!!! ${PORT}`));
   } catch (error) {
--- configs/routing.js
+++ configs/routing.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 const User = require("./mongo/model");
 
 function startRouter(app) {
-  router.get("/", async (req, res) => {
-    try {
-      console.log("In der Route /");
-      return res.json({ key: "bla" });
-    } catch (error) {
-      return res.status(500).json(error);
-    }
-  });
-
   router.post("/signup", async (req, res) => {
     try {
-      console.log("In der Route /signup");
-      await User.create({ name: "Peter", password: "Fox" });
-      return res.status(200).json({message: "signed up"});
+      const { name, password } = req.body;
+      if (!name || !password)
+        return res.status(400).json({ message: "Das gibets doch nich!" });
+      const newUser = User.findOne({ name });
+      if (!newUser)
+        return res.status(400).json({ message: "Das gibets och nich!" });
+
+      // brcrypt
+      const salt = bcrypt.genSalt(10);
+      const hashedPassword = bcrypt.hash(password, salt);
+
+      await User.create({ name: "Peter", password: hashedPassword });
+      return res.status(200).json({ message: "signed up" });
     } catch (error) {
       return res.status(500).json(error);
     }
@@ -25,8 +27,19 @@ function startRouter(app) {
 
   router.post("/login", async (req, res) => {
     try {
-      console.log("In der Route /login");
-      return res.status(200).json({message: "logged in"});
+      const { name, password } = req.body;
+      if (!name || !password)
+        return res.status(400).json({ message: "Das gibets doch nich!" });
+
+      // brcrypt
+      const user = User.findOne({ name });
+      const isValidUser = bcrypt.compare(password, user.password);
+      if (isValidUser) {
+        const validUser = { name: user.name, _id: user._id };
+        req.session.user = validUser;
+        return res.status(200).json({ message: "logged in" });
+      }
+      return res.status(400).json({ message: "nö" });
     } catch (error) {
       return res.status(500).json(error);
     }
